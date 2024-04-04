f5c4f9c4a4f70e49276dd25bf7962f1e78ebed40
Antonio Vállez <89712714+AntobioNallez@users.noreply.github.com>
2024-04-04 19:31:00 +0200

    Documentacion de la clase Curso

--- src/main/java/instituto/Curso.java
+++ src/main/java/instituto/Curso.java
@@ -7,23 +7,50 @@ package instituto;
 import java.util.TreeSet;
 
 /**
- *
+ * Clase que representa un curso de un instituto, con un nombre y una lista de
+ * alumnos.
+ * Se utiliza un TreeSet para almacenar los alumnos de forma ordenada.
+ * 
  * @author ProfDiurno
  */
 public class Curso {
 
+    /**
+     *  Nombre del curso
+     */
     private String nombre;
+
+    /** 
+     * Lista de alumnos del curso
+     */
     private TreeSet<Persona> listaAlumnos;
 
+    /**
+     * Método usado para obtener el nombre del curso.
+     * 
+     * @return Nombre del curso
+     */
     protected String getNombre() {
         return nombre;
     }
 
+    /**
+     * Constructor de la clase Curso que recibe el nombre del curso.
+     * Inicializa la lista de alumnos.
+     * 
+     * @param nombre Nombre del curso
+     */
     public Curso(String nombre) {
         this.nombre = nombre;
         listaAlumnos = new TreeSet<>();
     }
 
+    /**
+     * Método toString que devuelve una representación en formato de cadena del
+     * curso junto con la lista de alumnos.
+     * 
+     * @return Cadena con formato donde aparece el nombre del curso y los alumnos que lo componen.
+     */
     @Override
     public String toString() {
         String s = "--------------------" + nombre + "-----------------\n";
@@ -35,6 +62,11 @@ public class Curso {
         return s;
     }
 
+    /**
+     * Método para añadir un alumno al curso.
+     * 
+     * @param p Alumno a añadir
+     */
     public void aniadirAlumno(Persona p) {
         listaAlumnos.add(p);
     }
